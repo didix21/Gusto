e7413be78b3d16035abf999e6dbc6d97cc6924a6
Dídac Coll <didac.coll@qustodio.com>
2023-10-04 17:14:00 +0200

    Add sort enum

--- Gusto/ContentView.swift
+++ Gusto/ContentView.swift
@@ -8,14 +8,43 @@
 import SwiftUI
 import SwiftData
 
+enum SortOption {
+    case name
+    case quality
+    case speed
+    case price
+}
 
 
 struct ContentView: View {
     @Environment(\.modelContext) var modelContext
     @State private var path = [Restaurant]()
+    @State private var sortOption: SortOption = .name
+
+    private var sortDescriptor: SortDescriptor<Restaurant> {
+        switch sortOption {
+        case .name:
+            return .init(\.name)
+        case .quality:
+            return .init(\.qualityRating, order: .reverse)
+        case .speed:
+            return .init(\.speedRating, order: .reverse)
+        case .price:
+            return .init(\.priceRating, order: .reverse)
+        }
+    }
 
     var body: some View {
         NavigationStack(path: $path) {
+            VStack {
+                Picker("Sort By", selection: $sortOption) {
+                    Text("Name").tag(SortOption.name)
+                    Text("Price").tag(SortOption.price)
+                    Text("Quality").tag(SortOption.quality)
+                    Text("Speed").tag(SortOption.speed)
+                }
+                .pickerStyle(SegmentedPickerStyle())
+            }
             RestaurantListView()
             .navigationDestination(for: Restaurant.self) { restaurant in
                 EditRestaurantView(restaurant: restaurant)
