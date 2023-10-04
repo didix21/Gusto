4705d875fd601191087a93ee0a67faa7886fa21e
Dídac Coll <didac.coll@qustodio.com>
2023-10-04 11:24:08 +0200

    First commit

---
+++ Gusto/GustoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GustoApp.swift
+//  Gusto
+//
+//  Created by Qustodio
+//  
+
+import SwiftUI
+
+@main
+struct GustoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
